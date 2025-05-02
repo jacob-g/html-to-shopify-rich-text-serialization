0734a5feb3121bba368ec67a6226e25020481abc
Jacob Goldberg <jgoldberg@blueboltsolutions.com>
2025-05-02 15:50:44 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,134 @@
+import { parseDocument } from 'htmlparser2';
+import { inlineNodeTypes, listChildNodeTypes, ShopifyRichTextHeadingNode, ShopifyRichTextInlineNode, ShopifyRichTextListChildNode, ShopifyRichTextNode, ShopifyRichTextRoot, ShopifyRichTextTextNode, ShopifyRichTextTopLevelNode, topLevelNodeTypes } from './models';
+import type Document from 'domhandler';
+
+type DocumentNode = ReturnType<typeof parseDocument>['childNodes'][number];
+type TextNode = Extract<DocumentNode, { type: 'text' }>;
+type TagNode = Extract<DocumentNode, { type: 'tag' }>;
+
+interface ParseContext {
+    bold?: boolean;
+    italic?: boolean;
+}
+
+const htmlTextNodeToShopifyNode = (node: TextNode, context: ParseContext): ShopifyRichTextTextNode => 
+    ({
+        type: "text",
+        value: node.data,
+        bold: context.bold,
+        italic: context.italic,
+    });
+
+const htmlNodeToShopifyNodes = (node: DocumentNode, context: ParseContext = {}): ShopifyRichTextNode[] => {
+    switch (node.type) {
+        case 'text':
+            return [htmlTextNodeToShopifyNode(node, context)];
+        case 'tag':
+            switch (node.name) {
+                case 'b':
+                case 'strong':
+                    return node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, { ...context, bold: true }));
+                case 'i':
+                case 'em':
+                    return node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, { ...context, italic: true }));
+                case 'li':
+                    return [
+                        {
+                            type: 'list-item',
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => listChildNodeTypes.includes(node.type)) as ShopifyRichTextListChildNode[]
+                        }
+                    ];
+                case 'ul':
+                    return [
+                        {
+                            type: 'list',
+                            listType: 'unordered',
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => node.type === 'list-item')
+                        }
+                    ];
+                case 'ol':
+                    return [
+                        {
+                            type: 'list',
+                            listType: 'ordered',
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => node.type === 'list-item')
+                        }
+                    ];
+                case 'a':
+                    return [
+                        {
+                            type: 'link',
+                            url: node.attribs['href'],
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => node.type === 'text') as ShopifyRichTextTextNode[]
+                        }
+                    ]
+                case 'p':
+                    return [
+                        {
+                            type: 'paragraph',
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h1':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 1,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h2':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 2,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h3':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 3,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h4':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 4,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h5':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 5,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+                case 'h6':
+                    return [
+                        {
+                            type: 'heading',
+                            level: 6,
+                            children: node.childNodes.flatMap(node => htmlNodeToShopifyNodes(node, context)).filter(node => inlineNodeTypes.includes(node.type)) as ShopifyRichTextInlineNode[]
+                        }
+                    ];
+            }
+    }
+}
+
+const htmlToShopifyRichText = (html: string): ShopifyRichTextRoot => {
+    const doc = parseDocument(html);
+    console.log(doc.childNodes);
+
+    return {
+        type: 'root',
+        children: doc.childNodes.flatMap(node => htmlNodeToShopifyNodes(node)).filter(node => topLevelNodeTypes.includes(node.type)) as ShopifyRichTextTopLevelNode[]
+    }
+};
+
+export default htmlToShopifyRichText;
---
+++ src/models.ts
@@ -0,0 +1,70 @@
+interface ShopifyRichTextNodeBase {
+    type: string;
+}
+
+export interface ShopifyRichTextRoot extends ShopifyRichTextNodeBase {
+    type: 'root';
+    children: ShopifyRichTextTopLevelNode[];
+}
+
+export interface ShopifyRichTextTextNode extends ShopifyRichTextNodeBase {
+    type: 'text';
+    value: string;
+    bold?: boolean;
+    italic?: boolean;
+}
+
+export interface ShopifyRichTextLinkNode extends ShopifyRichTextNodeBase {
+    type: 'link';
+    url: string;
+    title?: string;
+    children?: ShopifyRichTextTextNode[];
+}
+
+export interface ShopifyRichTextHeadingNode extends ShopifyRichTextNodeBase {
+    type: 'heading';
+    level: 1 | 2 | 3 | 4 | 5 | 6;
+    children?: ShopifyRichTextInlineNode[];
+}
+
+interface ShopifyRichTextParagraphNode extends ShopifyRichTextNodeBase {
+    type: 'paragraph';
+    children?: ShopifyRichTextInlineNode[];
+}
+
+interface ShopifyRichTextListNode extends ShopifyRichTextNodeBase {
+    type: 'list';
+    listType: 'ordered' | 'unordered';
+    children?: ShopifyRichTextListItemNode[];
+}
+
+interface ShopifyRichTextListItemNode extends ShopifyRichTextNodeBase {
+    type: 'list-item';
+    children?: ShopifyRichTextListChildNode[];
+}
+
+export const inlineNodeTypes = ['text', 'link'];
+export const topLevelNodeTypes = ['heading', 'paragraph', 'list'];
+export const listChildNodeTypes = ['text', 'heading'];
+
+export type ShopifyRichTextNode =
+    | ShopifyRichTextTextNode
+    | ShopifyRichTextLinkNode
+    | ShopifyRichTextHeadingNode
+    | ShopifyRichTextParagraphNode
+    | ShopifyRichTextListNode
+    | ShopifyRichTextListItemNode
+    ;
+
+export type ShopifyRichTextTopLevelNode = 
+    ShopifyRichTextHeadingNode |
+    ShopifyRichTextParagraphNode |
+    ShopifyRichTextListNode;
+
+export type ShopifyRichTextInlineNode =
+    ShopifyRichTextTextNode |
+    ShopifyRichTextLinkNode;
+
+export type ShopifyRichTextListChildNode =
+    | ShopifyRichTextInlineNode
+    | ShopifyRichTextHeadingNode;
